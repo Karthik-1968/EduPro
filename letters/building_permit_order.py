e6cd4d18860496c841065e2f7bd8bf59d4673ffe
Karthik-1968 <gudapatiyeshwanth@gmail.com>
2025-01-27 08:30:00 +0000

    Commit

---
+++ letters/building_permit_order.py
@@ -0,0 +1,7 @@
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, Table, TableStyle
+from reportlab.lib.styles import ParagraphStyle
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.units import inch
+from reportlab.lib import colors
+
+def get_
--- letters/registration_for_building_construction.py
+++ letters/registration_for_building_construction.py
@@ -8,6 +8,29 @@ doc = SimpleDocTemplate("registration_for_building_construction.pdf", pagesize=l
 
 story = []
 
+image_path = "https://crm-backend-media-static.s3.ap-south-1.amazonaws.com/alpha/media/tgbpass_logo.png"
+title = "OFFICE OF THE GREATER HYDERABAD MUNICIPAL CORPORATION"
+small_text = "TG-bPASS"
+
+image_flowable = Image(image_path, width=1.5 * inch, height=1.5 * inch)
+image_flowable.hAlign = 'LEFT'
+
+title_flowable = Paragraph(title, ParagraphStyle(name='TitleStyle', fontSize=10, fontName='Helvetica-Bold'))
+title_flowable.hAlign = 'CENTER'
+
+small_text_flowable = Paragraph(small_text, ParagraphStyle(name='SmallTextStyle', fontSize=10, fontName='Helvetica-Bold', textColor=colors.green))
+small_text_flowable.hAlign = 'RIGHT'
+
+header_data = [
+    [image_flowable, title_flowable, small_text_flowable]
+]
+header_table = Table(header_data, colWidths=[1.5 * inch, 3 * inch, 2 * inch])
+header_table.setStyle(TableStyle([('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+                                    ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+                                    ('BOTTOMPADDING', (0, 0), (-1, -1), 12)]))
+story.append(header_table)
+
+story.append(Spacer(1, 12))
 text_style = ParagraphStyle(
     name='Normal',
     fontSize=10,
@@ -29,7 +52,6 @@ table_data = [
 
 table = Table(table_data)
 table.setStyle(TableStyle([
-    ('BACKGROUND', (0, 0), (-1, -1), colors.white),
     ('TEXTCOLOR', (0, 0), (1, 0), colors.black),
     ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
     ('FONTNAME', (0, 0), (0, 1), 'Helvetica-Bold'),
@@ -93,11 +115,9 @@ data = [
     ['3', 'Construction to be Completed', 'Before 13-11-2027'],
 ]
 
-table = Table(data, colWidths=[50, 200, 100])
+table = Table(data, colWidths=[50, 200, 200])
 
 style = TableStyle([
-    ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
-    ('BACKGROUND', (0, 0), (-1, -1), colors.white),
     ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
     ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
     ('FONTNAME', (0, 4), (-1, 4), 'Helvetica-Bold'),
@@ -112,5 +132,49 @@ style = TableStyle([
 
 table.setStyle(style)
 story.append(table)
+story.append(Spacer(1, 12))
 
-doc.build(story)
+conditions = [
+    "1. The certificate issued does not confer upon any ownership rights over the property. The applicant is solely responsible for legal litigations or disputes if any.",
+    "2. The extent/area of this plot is not more than 70.0 Sq. Yards and the plot is not part of a bigger plot which has been split for this purpose.",
+    "3. The applicant should not construct more than Ground Floor. Any further construction beyond will be liable for penalty / demolition.",
+    "4. The applicant shall not violate the front set back of 1.5mts and no structure / balcony projection is allowed in the setback area.",
+    "5. If the abutting road width is less than 30 feet (9.00 Mts.), 15 feet (4.5 Mts.) from the center of the existing road has to be left for road widening as per rule 4 (a) of G.O. Ms. No. 168 MA&UD dt:07-04-2012 or required area affected under Master Plan road and in case of plot abutting to MP / RDP / SRDP roads the extent of affected portion of land should be left in addition to the front setback.",
+    "6. The proposed construction should be in conformity with the Master plan, Land use and Zoning Regulations.",
+    "7. The proposed site should not be a part of Government land / Prohibited land / ULC Land / Disputed land / Municipal land / Layout Open space / Water bodies / NALA / Earmarked Parks and playgrounds not passing through High Tension/Electricity lines.",
+    "8. No building activity shall be carried out in certain areas as per rule 3 of G.O. Ms. No. 168 MA&UD dt:07-04-2012.",
+    "9. The applicant should commence the construction within 6 months and should complete within a period of 3 years from date of registration.",
+    "10. The registration shall stand lapsed if the construction is not completed within stipulated period and a fresh application shall have to be submitted.",
+    "11. Post verification will be carried out as per GHMC TG-bPASS Act and action will be initiated if any violation or misrepresentation of the facts is found.",
+    "12. Applicant is liable for penal action as per GHMC TG-bPASS Act, in case of misrepresentation or false declaration during post verification. Further the Certificate of Registration will be revoked and construction there upon will be demolished as per the provisions of the GHMC TG-bPASS Act.",
+    "13. In case if the building is constructed in deviation to the Building permission, the construction made in deviation to the Building Permission will be demolished as per the provisions of the GHMC TG-bPASS Act.",
+    "14. The applicant shall commence the construction of the building as per the Building Rules in force only after the completion of and clearance from the Post Verification process.",
+    "15. The issued proceedings are valid for any financial assistance / loan from financial institutions."
+]
+
+story.append(Paragraph("""The Certificate of Registration for building construction is issued subject to the
+                following conditions:""", ParagraphStyle(name='Title', fontSize=12, textColor=colors.black, fontName='Helvetica-Bold')))
+story.append(Spacer(1, 12))
+for condition in conditions:
+    story.append(Paragraph(condition, ParagraphStyle(name='Title', fontSize=10, textColor=colors.black, fontName='Helvetica')))
+    story.append(Spacer(1, 2))
+story.append(Spacer(1, 12))
+story.append(Paragraph("""NOTE: This is computer generated letter, doesn't require any manual signature""", \
+                      ParagraphStyle(name='Title', fontSize=12, textColor=colors.black, fontName='Helvetica', alignment=1)))
+story.append(Spacer(1, 12))
+def add_watermark(canvas, doc):
+
+    image_path = "https://crm-backend-media-static.s3.ap-south-1.amazonaws.com/alpha/media/tgbpass_logo.png"
+
+    img_width = 400
+    img_height = 400
+
+    page_width, page_height = letter
+    x_position = (page_width - img_width) / 2
+    y_position = (page_height - img_height) / 2
+
+    canvas.setFillAlpha(0.3)
+
+    canvas.drawImage(image_path, x_position, y_position, width=img_width, height=img_height, mask='auto')
+
+doc.build(story, onFirstPage=add_watermark, onLaterPages=add_watermark)
